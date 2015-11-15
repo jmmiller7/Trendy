d1b933a8300254d58c45d700adbe0494ba1e8bd3
Jake Miller <jmmiller@pio.carrollu.edu>
2015-11-14 18:06:38 -0600

    Change all ImageView views to GlideView

--- app/src/main/java/com/example/jmmil/trendy/GalleryFragment.java
+++ app/src/main/java/com/example/jmmil/trendy/GalleryFragment.java
@@ -5,12 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
+import android.view.View.OnClickListener;
 
-/**
- * Created by jmmil on 11/8/2015.
- */
+import com.bumptech.glide.Glide;
+
 public class GalleryFragment extends Fragment {
+
+    GlideView glideView1;
+    GlideView glideView2;
+    GlideView glideView3;
+    GlideView glideView4;
+    GlideView glideView5;
+    GlideView glideView6;
+    GlideView glideView7;
+    GlideView glideView8;
+    GlideView glideView9;
+
+    GlideView previewGlideView;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -18,6 +30,65 @@ public class GalleryFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
+        glideView1 = (GlideView) view.findViewById(R.id.glideView1);
+        glideView1.setOnClickListener(glideViewListener);
+        glideView2 = (GlideView) view.findViewById(R.id.glideView2);
+        glideView2.setOnClickListener(glideViewListener);
+        glideView3 = (GlideView) view.findViewById(R.id.glideView3);
+        glideView3.setOnClickListener(glideViewListener);
+        glideView4 = (GlideView) view.findViewById(R.id.glideView4);
+        glideView4.setOnClickListener(glideViewListener);
+        glideView5 = (GlideView) view.findViewById(R.id.glideView5);
+        glideView5.setOnClickListener(glideViewListener);
+        glideView6 = (GlideView) view.findViewById(R.id.glideView6);
+        glideView6.setOnClickListener(glideViewListener);
+        glideView7 = (GlideView) view.findViewById(R.id.glideView7);
+        glideView7.setOnClickListener(glideViewListener);
+        glideView8 = (GlideView) view.findViewById(R.id.glideView8);
+        glideView8.setOnClickListener(glideViewListener);
+
+        glideView9 = (GlideView) view.findViewById(R.id.glideView9);
+        glideView9.setOnClickListener(glideViewListener);
+
+        previewGlideView = (GlideView) view.findViewById(R.id.previewGlideView);
+
+        loadGlide();
+
         return view;
     }
+
+    private void loadGlide() {
+
+        glideView1.setGlideView("https://media0.giphy.com/media/3rgXBDGArplt1PJodi/200w.gif");
+
+        glideView2.setGlideView("https://media2.giphy.com/media/TSeUOygJDayVG/200w.gif");
+
+        glideView3.setGlideView("https://media2.giphy.com/media/dpYkMt1b9BNmw/200w.gif");
+
+        glideView4.setGlideView("https://media0.giphy.com/media/3rgXBDGArplt1PJodi/200w.gif");
+
+        glideView5.setGlideView("https://media2.giphy.com/media/TSeUOygJDayVG/200w.gif");
+
+        glideView6.setGlideView("https://media2.giphy.com/media/dpYkMt1b9BNmw/200w.gif");
+
+        glideView7.setGlideView("https://media0.giphy.com/media/3rgXBDGArplt1PJodi/200w.gif");
+
+        glideView8.setGlideView("https://media2.giphy.com/media/TSeUOygJDayVG/200w.gif");
+
+        glideView9.setGlideView("https://media0.giphy.com/media/m6EMx4ZWHaSUU/200w.gif");
+
+        previewGlideView.setGlideView("https://media2.giphy.com/media/dpYkMt1b9BNmw/200w.gif");
+
+    }
+
+    private OnClickListener glideViewListener = new OnClickListener(){
+
+        public void onClick(View v) {
+
+            GlideView gv = (GlideView) v;
+
+            // change previewGlideView to url that is in gv
+            previewGlideView.setGlideView(gv.getURL());
+        }
+    };
 }
--- app/src/main/java/com/example/jmmil/trendy/GlideView.java
+++ app/src/main/java/com/example/jmmil/trendy/GlideView.java
@@ -1,7 +1,59 @@
 package com.example.jmmil.trendy;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 /**
  * Created by jmmil on 11/14/2015.
  */
-public class GlideView {
+public class GlideView extends ImageView{
+
+    String url;
+    Context context;
+
+    public GlideView(Context context)
+    {
+        super(context);
+        this.context = context;
+    }
+
+    public GlideView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        this.context = context;
+    }
+
+    public GlideView(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        // TODO Auto-generated method stub
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+
+    }
+
+    public String getURL(){
+        return url;
+    }
+
+    public void setGlideView(String url){
+        this.url = url;
+        Glide.with(context)
+                .load(url)
+                .into(this);
+    }
 }
