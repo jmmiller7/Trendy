db140852f18c2756083423468c23a15269adf092
Henry Snopek <hhsnopek@gmail.com>
2015-12-05 07:07:03 -0600

    update theme, adjust code style, add imgur api

--- app/src/main/java/com/example/jmmil/trendy/SimpleGalleryFragment.java
+++ app/src/main/java/com/example/jmmil/trendy/SimpleGalleryFragment.java
@@ -9,100 +9,93 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 public class SimpleGalleryFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private int NUM_COLUMNS=3;
-
-    String[] imageIDs;
+    private int NUM_COLUMNS = 3;
+    private View view;
+    public String[] imageIDs = new String[]{
+            "http://media2.giphy.com/media/1XoXJkOGri6Hu/200_d.gif",
+            "http://media0.giphy.com/media/6Lt0PMyvjTX8c/200_d.gif",
+            "http://media1.giphy.com/media/xTiTnJxChUjF4u958A/200_d.gif",
+            "http://media3.giphy.com/media/MJs7EYwHyG8XC/200_d.gif",
+            "http://media1.giphy.com/media/14rACYMwBmXDqM/200_d.gif",
+            "http://media2.giphy.com/media/3XtigW1mlKBW0/200_d.gif",
+            "http://media1.giphy.com/media/xTiTnLRQtRkaRKkKWs/200_d.gif"
+    };
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.simple_fragment_gallery, container, false);
 
-        imageIDs = populateURLS();
+        view = inflater.inflate(R.layout.simple_fragment_gallery, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+
         mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), NUM_COLUMNS));
         mRecyclerView.setAdapter(new GalleryFragmentAdapter());
 
         return view;
     }
 
-
-    private String[] populateURLS(){
-        String[] imageIDs = {
-                "http://media2.giphy.com/media/1XoXJkOGri6Hu/200_d.gif",
-                "http://media0.giphy.com/media/6Lt0PMyvjTX8c/200_d.gif",
-                "http://media1.giphy.com/media/xTiTnJxChUjF4u958A/200_d.gif",
-                "http://media3.giphy.com/media/MJs7EYwHyG8XC/200_d.gif",
-                "http://media1.giphy.com/media/14rACYMwBmXDqM/200_d.gif",
-                "http://media2.giphy.com/media/3XtigW1mlKBW0/200_d.gif",
-                "http://media1.giphy.com/media/xTiTnLRQtRkaRKkKWs/200_d.gif",
-                "http://media2.giphy.com/media/3oEduZkX5W1nW0PqOk/200_d.gif",
-                "http://media2.giphy.com/media/l41lJa0rg1KO3nZAs/200_d.gif",
-                "http://media1.giphy.com/media/xTiTnybAG7cYyzzvQk/200_d.gif",
-                "http://media4.giphy.com/media/3oEduSLalG3rotykI8/200_d.gif",
-                "http://media3.giphy.com/media/l41lSxrJMaGyxyBpu/200_d.gif",
-                "http://media3.giphy.com/media/n8jpGuNug3LIQ/200_d.gif"
-        };
-
-        return imageIDs;
+    public void updateView(String[] images) {
+        imageIDs = images;
+//        delete mRecyclerView.getAdapter();
     }
-
-
 
     private OnClickListener glideViewListener = new OnClickListener(){
 
         public void onClick(View v) {
+        GlideView glideView = (GlideView) v;
+        String url = glideView.getURL();
+        Intent intent = new Intent(SimpleGalleryFragment.this.getContext(),
+                SelectedItemActivity.class);
 
-            //TODO
-            GlideView gv = (GlideView) v;
-            String url = gv.getURL();
-
-            Intent intent = new Intent(SimpleGalleryFragment.this.getContext(), SelectedItemActivity.class);
-            intent.putExtra("url", url);
-            startActivity(intent);
+        intent.putExtra("url", url);
+        startActivity(intent);
         }
     };
 
+    public int getImageIDAmt () { return imageIDs.length; }
 
-    public class GalleryFragmentAdapter extends RecyclerView.Adapter<GlideViewHolder>
-    {
+
+    public class GalleryFragmentAdapter extends RecyclerView.Adapter<GlideViewHolder> {
         @Override
-        public GlideViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-        {
+        public GlideViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             GlideView gv = new GlideView(getContext());
             gv.setOnClickListener(glideViewListener);
             return new GlideViewHolder(gv);
         }
 
         @Override
-        public void onBindViewHolder(GlideViewHolder holder, int position)
-        {
+        public void onBindViewHolder(GlideViewHolder holder, int position) {
             String uri = imageIDs[position];
             holder.setImageURL(uri);
         }
 
         @Override
-        public int getItemCount()
-        {
-            return imageIDs.length;
-        }
+        public int getItemCount() { return imageIDs.length; }
+
+//        public void clearData() {
+//            int size = imageIDs.size();
+//            if (size > 0) {
+//                for (int i = 0; i < size; i++) {
+//                    imageIDs.remove(0);
+//                }
+//
+//                this.notifyItemRangeRemoved(0, size);
+//            }
+//        }
     }
 
     public class GlideViewHolder extends RecyclerView.ViewHolder
     {
         private GlideView mGlideView;
 
-        public GlideViewHolder(View itemView)
-        {
+        public GlideViewHolder(View itemView) {
             super(itemView);
             mGlideView = (GlideView) itemView;
-
         }
 
         public void setImageURL(String imageURL)
